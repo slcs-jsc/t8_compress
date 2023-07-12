35c307513c3c572b518ebde4dd8eef49fe890848
Lars Hoffmann <l.hoffmann@fz-juelich.de>
2023-07-12 20:59:18 +0200

    Modified main function.

--- t8_compress_v2.cxx
+++ t8_compress_v2.cxx
@@ -307,36 +307,23 @@ void t8_interpolation(
   T8_FREE(elem_data);
 }
 
-int t8_step7_main(
-  int argc,
-  char **argv) {
-
-  int mpiret;
-
-  /* Initialize MPI. This has to happen before we initialize sc or t8code. */
-  mpiret = sc_MPI_Init(&argc, &argv);
-  /* Error check the MPI return value. */
-  SC_CHECK_MPI(mpiret);
-
-  /* Initialize the sc library, has to happen before we initialize t8code. */
-  sc_init(sc_MPI_COMM_WORLD, 1, 1, NULL, SC_LP_PRODUCTION);
-  /* Initialize t8code with log level SC_LP_PRODUCTION. See sc.h for more info on the log levels. */
-  t8_init(SC_LP_PRODUCTION);
-
-  /* Create forest, define data on forest, adapt forest, interpolate data */
-  t8_interpolation();
-
-  sc_finalize();
-
-  mpiret = sc_MPI_Finalize();
-  SC_CHECK_MPI(mpiret);
-
-  return 0;
-}
-
 int main(
   int argc,
   char **argv) {
 
-  return t8_step7_main(argc, argv);
+  /* Initialize... */
+  int mpiret = sc_MPI_Init(&argc, &argv);
+  SC_CHECK_MPI(mpiret);
+  sc_init(sc_MPI_COMM_WORLD, 1, 1, NULL, SC_LP_PRODUCTION);
+  t8_init(SC_LP_PRODUCTION);
+  
+  /* Create forest, define data on forest, adapt forest, interpolate data */
+  t8_interpolation();
+
+  /* Finalize... */
+  sc_finalize();
+  mpiret = sc_MPI_Finalize();
+  SC_CHECK_MPI(mpiret);
+
+  return 0;
 }
